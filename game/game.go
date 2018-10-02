6f0937f62639f82d02f52afebba91a6ecc7e4e77
florian <flbe@hooq.tv>
2018-10-02 18:44:34 +0800

    Revert "Third test green but wrong"
    This reverts commit ee303c131c4e81dfedd297521ffd9bc02f078b8a.

--- game/game.go
+++ game/game.go
@@ -7,14 +7,7 @@ type Game struct {
 
 func (game *Game) GetScore() int {
 	sum := 0
-	// TODO: Index only until 19 is wrong!
-	for i := 0; i < 19; i++ {
-		// TODO this is not the correct way to detect a spare!
-		isSpare := game.rolls[i]+game.rolls[i+1] == 10
-		if isSpare {
-			// This is not a good way to add the bonus, hard to understand!
-			sum += game.rolls[i+2]
-		}
+	for i := 0; i < 21; i++ {
 		sum += game.rolls[i]
 	}
 	return sum
