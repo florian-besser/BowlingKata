bf98dd420d1934ab1069d53d6d885fc01879292c
florian <flbe@hooq.tv>
2018-10-02 14:55:56 +0800

    Second test refactor to rolls part I

--- game/game.go
+++ game/game.go
@@ -1,13 +1,21 @@
 package game
 
 type Game struct {
-	sum int
+	rolls      [21]int
+	rollsIndex int
+	sum        int
 }
 
 func (game *Game) GetScore() int {
-	return game.sum
+	sum := 0
+	for i := 0; i < 21; i++ {
+		sum += game.rolls[i]
+	}
+	return sum
 }
 
 func (game *Game) Roll(roll int) {
+	game.rolls[game.rollsIndex] = roll
+	game.rollsIndex++
 	game.sum += roll
 }
