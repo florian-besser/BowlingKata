adcb049ae885adfd7464e659bdfb8a0eb86cea70
florian <flbe@hooq.tv>
2018-10-02 18:47:26 +0800

    Test 3 refactor, extract meaningful variable name

--- game/game.go
+++ game/game.go
@@ -9,7 +9,8 @@ func (game *Game) GetScore() int {
 	sum := 0
 	index := 0
 	for frame := 0; frame < 10; frame++ {
-		if game.rolls[index]+game.rolls[index+1] == 10 {
+		isSpare := game.rolls[index]+game.rolls[index+1] == 10
+		if isSpare {
 			sum += game.rolls[index] + game.rolls[index+1] + game.rolls[index+2]
 		} else {
 			sum += game.rolls[index] + game.rolls[index+1]
