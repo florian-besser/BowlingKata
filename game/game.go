7c55f0989127973cdbcf9b1b35152014101a780b
florian <flbe@hooq.tv>
2018-10-02 18:48:53 +0800

    Test 4 green

--- game/game.go
+++ game/game.go
@@ -9,13 +9,18 @@ func (game *Game) GetScore() int {
 	sum := 0
 	index := 0
 	for frame := 0; frame < 10; frame++ {
+		isStrike := game.rolls[index] == 10
 		isSpare := game.rolls[index]+game.rolls[index+1] == 10
-		if isSpare {
+		if isStrike {
 			sum += game.rolls[index] + game.rolls[index+1] + game.rolls[index+2]
+			index++
+		} else if isSpare {
+			sum += game.rolls[index] + game.rolls[index+1] + game.rolls[index+2]
+			index += 2
 		} else {
 			sum += game.rolls[index] + game.rolls[index+1]
+			index += 2
 		}
-		index += 2
 	}
 	return sum
 }
